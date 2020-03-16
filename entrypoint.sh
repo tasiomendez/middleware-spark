e3eba47c02c1f12363a985bb5ffb2d1c1f517e2d
Tasio Mendez <tm.tasio@gmail.com>
2020-03-16 01:44:39 +0100

    Using hadoop for sharing files

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,23 @@
 APPLICATION_JAR_FILE=$1
 shift
 
+# Configuration of hadoop host
+if [ -z "${HADOOP_HOST}" ]; then
+  export HADOOP_HOST=hadoop
+fi
+
+if [ -z "${HADOOP_PORT}" ]; then
+  export HADOOP_PORT=9000
+fi
+
+$HADOOP_HOME/bin/hdfs dfs -fs hdfs://$HADOOP_HOST:$HADOOP_PORT -mkdir /spark
+$HADOOP_HOME/bin/hdfs dfs -fs hdfs://$HADOOP_HOST:$HADOOP_PORT -put $JAVA_PROJECT_DATASET /spark/dataset.csv
+
 $SPARK_HOME/bin/spark-submit \
   --master spark://$SPARK_MASTER_HOST:$SPARK_MASTER_PORT \
   --deploy-mode client \
   $SPARK_SUBMIT_ARGS \
   $APPLICATION_JAR_FILE \
   --master spark://$SPARK_MASTER_HOST:$SPARK_MASTER_PORT \
-  --file $JAVA_PROJECT_DATASET \
+  --file hdfs://$HADOOP_HOST:$HADOOP_PORT/spark/dataset.csv \
   $@
