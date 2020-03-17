6a6084ead8d6c5f05df8eefbed9af5559c2d4f61
Tasio Mendez <tm.tasio@gmail.com>
2020-03-17 18:33:55 +0100

    Fixing networking issues

--- docker/worker/entrypoint.sh
+++ docker/worker/entrypoint.sh
@@ -13,12 +13,6 @@ if [ "$SPARK_WORKER_HOST" = "" ]; then
   SPARK_WORKER_HOST=$(hostname -I)
 fi
 
-if [ "$SPARK_DRIVER_HOST" = "" ]; then
-  SPARK_DRIVER_HOST=$(hostname -I)
-fi
-
-sed -i "s|{{SPARK_DRIVER_HOST}}|$SPARK_DRIVER_HOST|g" $SPARK_CONF_DIR/spark-defaults.conf
-
 $SPARK_HOME/bin/spark-class org.apache.spark.deploy.worker.Worker \
   --host $SPARK_WORKER_HOST \
   --webui-port $SPARK_WORKER_WEBUI_PORT \
